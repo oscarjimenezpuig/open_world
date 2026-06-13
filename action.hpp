5f82dea411aecad1904eb32654a2b0fb52e133a4
oscarjimenezpuig <44938488+oscarjimenezpuig@users.noreply.github.com>
2026-06-13 23:46:11 +0200

    Add files via upload

---
+++ action.hpp
@@ -0,0 +1,17 @@
+// MUNDO LIBRE 13-6-26
+
+//se definen lo que es una accion y se guardan todas las acciones en un array
+
+#include "basic.hpp"
+
+typedef short (*Function)(vector<short>);
+
+class Action {
+    private:
+        vector<std::string> str; //conjunto de cadenas que representa una funcion
+        Function fnc;
+    public:
+
+};
+
+
